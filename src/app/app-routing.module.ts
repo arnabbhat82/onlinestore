3be064b1a5541e544d0fd1c6e4ccfb4c25381209
arnabbhat82 <arnabbhat@gmail.com>
2019-09-10 16:51:26 +0530

    login routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,17 @@ import { AboutusComponent } from './shared/aboutus/aboutus.component';
 import { SettingsComponent } from './settings/settings/settings.component';
 import { AdmintabComponent } from './admin/admintab/admintab.component';
 import { ProductComponent } from './user/product/product.component';
+import { LoginComponent } from './user/login/login.component';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/aboutus', pathMatch: 'full'},
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'aboutus', component: AboutusComponent},
   {path: 'admin', component: AdmintabComponent},
   {path: 'product', component: ProductComponent},
   {path: 'settings', component: SettingsComponent},
-  {path: '**', redirectTo: '/aboutus', pathMatch: 'full'}
+  {path: 'login', component: LoginComponent},
+  {path: '**', redirectTo: '/login', pathMatch: 'full'}
 
 ];
 
